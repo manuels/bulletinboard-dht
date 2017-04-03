b3ef4839bf82f252bf85398c515a3b8786b530aa
Manuel Schölling <manuel.schoelling@gmx.de>
2017-04-03 22:10:10 +0200

    CI: install dbus

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -18,10 +18,11 @@ main() {
     test -f Cargo.lock || cargo generate-lockfile
 
     # TODO Update this to build the artifacts that matter to you
-    cross rustc --bin hello --target $TARGET --release -- -C lto
+    #cross rustc --bin bulletinboard --target $TARGET --release -- -C lto
+    cross build --target $TARGET --no-default-features --release
 
     # TODO Update this to package the right artifacts
-    cp target/$TARGET/release/hello $stage/
+    cp target/$TARGET/release/bulletinboard $stage/
 
     cd $stage
     tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
--- ci/script.sh
+++ ci/script.sh
@@ -4,18 +4,15 @@ set -ex
 
 # TODO This is the "test phase", tweak it as you see fit
 main() {
-    cross build --target $TARGET
-    cross build --target $TARGET --release
+    cross build --target $TARGET --no-default-features
+    cross build --target $TARGET --release --no-default-features
 
     if [ ! -z $DISABLE_TESTS ]; then
         return
     fi
 
-    cross test --target $TARGET
-    cross test --target $TARGET --release
-
-    cross run --target $TARGET
-    cross run --target $TARGET --release
+    cross test --target $TARGET --no-default-features
+    cross test --target $TARGET --release --no-default-features
 }
 
 # we don't run the "test phase" when doing deploys
